b956e67b2d777ee2aceb59cba5be9084b955c903
Sebastian Roblez <roblez769@gmail.com>
2025-10-22 19:09:57 -0500

    Agregar archivos de configuración y estructura inicial del proyecto para Spring Boot

---
+++ bankapp/bankapp/src/main/java/com/ntt/challenge/bankapp/BankappApplication.java
@@ -0,0 +1,13 @@
+package com.ntt.challenge.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankappApplication.class, args);
+	}
+
+}
